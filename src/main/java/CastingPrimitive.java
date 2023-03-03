29a85c691515360a5de603284c93dca1445d9031
gduke93 <grayduke93@gmail.com>
2023-03-03 22:58:40 +0000

    Completed Lab Casting

--- src/main/java/CastingPrimitive.java
+++ src/main/java/CastingPrimitive.java
@@ -16,7 +16,7 @@ public class CastingPrimitive {
      * @return the int form of d.
      */
     public int castToInt(double d){
-        //double d = (int) d;
+        
         return (int) d;
     }
 }
